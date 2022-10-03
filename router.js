65dc308e78bdf1d7cf8f2c0e1e7d6e7b0fdecc0d
Jose Tomas Hernandez Lopez <uniigooo@gmail.com>
2022-10-03 10:12:19 +0200

    maintenance-mode

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/maintenance.html'));
 });
 router.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, '/public/login.html'));
