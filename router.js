068c8a31c9371fa8f798ea787ff6b0790a537be9
Jose Tomas Hernandez Lopez <uniigooo@gmail.com>
2022-10-03 10:14:21 +0200

    index and maintenance

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/maintenance.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 router.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, '/public/login.html'));
