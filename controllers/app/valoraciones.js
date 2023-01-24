69b4870561fa582da01d38087d88d168eaac53ee
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2023-01-24 10:55:18 +0100

    Update valoraciones.js

--- controllers/app/valoraciones.js
+++ controllers/app/valoraciones.js
@@ -1,9 +1,9 @@
 const con = require("../database");
 
 const obtenerValoraciones = async (req, res) => {
-    console.log('req.params :>> ', req.params);
+    console.log('req.params :>> ', req.user);
     con.execute(
-        'SELECT * FROM valoraciones WHERE from_user = ? OR to_user = ?;', [req.params._id, req.params._id], (err, result) => {
+        'SELECT * FROM valoraciones WHERE from_user = ? OR to_user = ?;', [req.user._id, req.user._id], (err, result) => {
             if (err) console.log(err);
             if(result.length === 0) {
                 return res.status(200).json({info: 'No hay reservas registradas.'});
