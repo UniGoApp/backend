2410915e5fa6fa26a577c422d846842a67362906
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2023-01-24 10:51:14 +0100

    Update valoraciones.js

--- controllers/app/valoraciones.js
+++ controllers/app/valoraciones.js
@@ -1,6 +1,7 @@
 const con = require("../database");
 
 const obtenerValoraciones = async (req, res) => {
+    console.log('req.params :>> ', req.params);
     con.execute(
         'SELECT * FROM valoraciones WHERE from_user = ? OR to_user = ?;', [req.params._id, req.params._id], (err, result) => {
             if (err) console.log(err);
