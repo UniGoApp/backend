2caf947d37cec7e982b1e1ecce52fbc952ce0c1f
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2023-01-24 11:37:38 +0100

    Update valoraciones.js

--- controllers/app/valoraciones.js
+++ controllers/app/valoraciones.js
@@ -1,15 +1,17 @@
 const con = require("../database");
 
 const obtenerValoraciones = async (req, res) => {
-    console.log('req.params :>> ', req.user);
     con.execute(
         'SELECT * FROM valoraciones WHERE from_user = ? OR to_user = ?;', [req.user._id, req.user._id], (err, result) => {
-            if (err) console.log(err);
+            if (err) {
+                console.log(err);
+                return res.status(200).json({error: true, info: 'Error inesperado en la base de datos.', data:''});
+            }
             if(result.length === 0) {
-                return res.status(200).json({info: 'No hay reservas registradas.'});
+                return res.status(200).json({error: true, info: 'No hay reservas registradas.', data:''});
             }else{
                 return res.status(200).json({
-                    msg: '',
+                    error: false,
                     data: result,
                     info: ''
                 });
