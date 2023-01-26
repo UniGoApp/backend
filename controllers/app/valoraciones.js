260ff11fa3973fad82846ab7e5c7ac1781f79ed6
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2023-01-26 16:50:19 +0100

    npm updates

--- controllers/app/valoraciones.js
+++ controllers/app/valoraciones.js
@@ -1,8 +1,9 @@
 const con = require("../database");
 
 const obtenerValoraciones = async (req, res) => {
+    const user_id = req.user._id;
     con.execute(
-        'SELECT * FROM valoraciones WHERE from_user = ? OR to_user = ?;', [req.user._id, req.user._id], (err, result) => {
+        'SELECT v.*, u.id, u.username, u.picture, u.email_confirmed FROM valoraciones AS v INNER JOIN usuarios AS u ON v.from_user=u.id OR v.to_user=u.id WHERE from_user = ? OR to_user = ?;', [user_id, user_id], (err, result) => {
             if (err) {
                 console.log(err);
                 return res.status(200).json({error: true, info: 'Error inesperado en la base de datos.', data:''});
