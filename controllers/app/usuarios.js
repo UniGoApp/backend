9997d20612afb9efe8f6215c4009bf3adebc6882
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2023-02-10 20:28:42 +0100

    Update usuarios.js

--- controllers/app/usuarios.js
+++ controllers/app/usuarios.js
@@ -1,29 +1,36 @@
 const con = require("../database");
 
 const obtenerUsuario = async (req, res) => {
-    if(req.auth._rol === "USER" && req.auth._id === req.params.id){
-        con.execute(
-            'SELECT * FROM `usuarios` WHERE `rol` = ? AND `id` = ?;', ["USER", req.params.id], (err, result) => {
-                if (err) throw err;
-                if(result.length === 0) return res.status(200).json({
-                    info: 'No existe el usuario solicitado.',
-                    data: '',
-                    error: false
-                });
-                return res.status(200).json({
-                    error: false,
-                    data: result,
-                    info: ''
-                });
-            }
-        );
-    }else{
-        return res.status(403).json({
-            info: 'Acceso no autorizado',
-            data: '',
-            error: true
-        });
-    }
+    const user_id = req.params.id;
+    con.execute(
+        'SELECT u.id_campus, u.bio, u.creation_time, c.name AS campus, c.university, c.region, c.icon FROM usuarios AS u LEFT JOIN campus AS c ON c.id=u.id_campus WHERE u.id=?;', [user_id], (err, result1) => {
+            if (err) throw err;
+            if(result1.length === 0) return res.status(200).json({
+                info: 'No existe el usuario solicitado.',
+                data: '',
+                error: true
+            });
+            con.execute(
+                'SELECT * FROM valoraciones WHERE to_user=?;', [user_id], (err, result2) => {
+                    if (err) throw err;
+                    con.execute(
+                        'SELECT * FROM viajes WHERE status=? AND id_user=?;', ['ACTIVO', user_id], (err, result3) => {
+                            if (err) throw err;
+                            return res.status(200).json({
+                                error: false,
+                                data: {
+                                    user: result1[0],
+                                    ratings: result2,
+                                    trips: result3
+                                },
+                                info: ''
+                            });
+                        }
+                    );
+                }
+            );
+        }
+    );
 };
 
 const modificarUsuario = async (req, res) => {
